c2631c39e91ae582cef7296861fdd683a0167248
David Nordeen <davidnordeen@gmail.com>
2019-04-30 10:35:13 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const express = require('express');
+const morgan = require('morgan');
+const store = require('./playstore');
+const cors = require('cors');
+const app = express();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+app
+  .use(morgan('dev'))
+  .use(cors());
+
+
+app.get('/apps', (req, res) => {
+  const { sort, genres } = req.query;
+  let results = store;
+
+  if (sort) {
+    if (!['rating', 'app'].includes(sort)) {
+      return res
+        .status(400)
+        .send('sort must be one of rating or app');
+    }
+  }
+  else if (sort === 'app') {
+    results = results.sort((a, b) => {
+      const x = a.app.toLowerCase();
+      const y = b.app.toLowerCase();
+      return x[sort] > y[sort] ? -1 : x[sort] < y[sort] ? 1 : 0;
+    })
+  }
+  else if (sort === 'rating') {
+    results = results.sort((a, b) => {
+      return a['rating'] < b['rating'] ? 1 : a['rating'] > b['rating'] ? -1 : 0;
+    })
+  }
+
+  if (genres) {
+    if (!['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'].includes(genres.toLowerCase())) {
+      return res.status(400).send('Genre must be one of Action, Puzzle, Strategy, Casual, Arcade or Card');
+    }
+    results = results.filter(app => {
+      return app.Genres.toLowerCase() === genres.toLowerCase();
+    });
+  }
+
+
+  return res.send(results);
+
+});
+  app.listen(8000, () => {
+  console.log('server started on port 8000');
+});
+
